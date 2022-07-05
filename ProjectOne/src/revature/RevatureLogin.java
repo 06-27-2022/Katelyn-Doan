69c401d33879f4620b3a27e4541bcd36850e8d98
kndoan <katelyn592@revature.net>
2022-07-05 18:18:19 -0400

    accidently did some things here

---
+++ ProjectOne/src/revature/RevatureLogin.java
@@ -0,0 +1,16 @@
+package revature;
+
+import java.util.Scanner;
+
+public class RevatureLogin {
+	
+	String[] storedUsers = new String[100];
+	String[] storedPass = new String[100];
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+
+	}
+
+}
