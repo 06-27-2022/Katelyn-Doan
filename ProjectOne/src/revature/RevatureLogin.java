a59689e066d128990df0f6cbf63e9803628984d2
kndoan <katelyn592@revature.net>
2022-07-11 10:13:08 -0400

    update on what I have been working on

--- ProjectOne/src/revature/RevatureLogin.java
+++ ProjectOne/src/revature/RevatureLogin.java
@@ -1,16 +1,74 @@
-package revature;
+package com.revature;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class RevatureLogin {
 	
-	String[] storedUsers = new String[100];
-	String[] storedPass = new String[100];
+	protected static List<String> storedUsernames = new ArrayList<>();
+	protected static List<String> storedPasswords = new ArrayList<>();
+	protected String username;
+	protected String password;
+	
+	public static void autheticated(String username, String password) {
+		if(storedUsernames.contains(username) && storedPasswords.contains(password)) {
+			System.out.println("Access granted");
+		}else {
+			System.out.println("Wrong answer");
+		}
+	}
 
 	public static void main(String[] args) {
 		
-		Scanner scanner = new Scanner(System.in);
+		storedUsernames.add("kndoan");
+		storedPasswords.add("potato");
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Please pick an option:");
+		System.out.println("1 - Login with existing account");
+		System.out.println("2 - Register for new account");
+		System.out.println("3 - Quit application");
+		byte choice = scan.nextByte();
+		
+		switch(choice) {
+		case 1:
+			System.out.print("Username: ");
+			String user = scan.next();
+			
+			System.out.print("Password: ");
+			String pass = scan.next();
+			
+			break;
+		
+		case 2:
+			System.out.println("Register blah blah");
+			break;
+		
+		case 3:
+			System.out.println("Quitting now . . . ");
+			scan.close();
+			
+			
+			
+//			if(storedUsernames.contains(user)) {
+//				System.out.println("Access granted");
+//			}
+			
 
+
+//			for(String i : storedUsernames) {
+//				if(storedUsernames.contains(user)){
+//					System.out.print("welcome " + user);
+//				}else {
+//					System.out.println("Wrong username or password");
+//				}
+//			}
+		
+		}
+
+		scan.close();
 	}
 
 }
+
